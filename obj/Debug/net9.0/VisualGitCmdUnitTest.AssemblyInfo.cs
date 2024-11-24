81c9082d14d8ba1dd6fae82dec03add238f7a273
Mark Howard <markjameshoward@hotmail.co.uk>
2024-11-24 17:22:21 +1300

    remove obj

--- obj/Debug/net9.0/VisualGitCmdUnitTest.AssemblyInfo.cs
+++ obj/Debug/net9.0/VisualGitCmdUnitTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisualGitCmdUnitTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ca1c940867e28d427cc24b51c70c6b19feb4809")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9522e0675292b8fa0dc491a1deaab6f02e08fee2")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisualGitCmdUnitTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisualGitCmdUnitTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
