0b3365c7b03c899b65586db1b587a6ea0184d9ef
Mark Howard <markjameshoward@hotmail.co.uk>
2025-01-05 16:36:22 +1300

    dll

--- obj/Debug/net9.0/VisualGitCmdUnitTest.AssemblyInfo.cs
+++ obj/Debug/net9.0/VisualGitCmdUnitTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisualGitCmdUnitTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7342d0be8632a7e8e6a137b75aef0b95a4f8075")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+405aea2afc8fe56abab4f902fe752a82c97f332a")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisualGitCmdUnitTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisualGitCmdUnitTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
